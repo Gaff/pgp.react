d47ec5c233a150e2ed2e5399d42edf52ed5197a5
EC2 Default User <ec2-user@ip-172-31-21-140.ec2.internal>
2020-05-14 05:38:33 +0000

    Some behaviour

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Form, FormControl } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
 
-export function Main() {
+export function Main(props : any) {
     
     const [getMessage, setMessage] = React.useState("");
     
-    const onMessageChange = (e: React.FormEvent<FormControl & HTMLInputElement>) => {
-        
+    const handleFocus = (event: any) => event.target.select();
+    
+    const onMessageChangeDebounced = AwesomeDebouncePromise(e=>e, 500);
+    const onMessageChange = async (text: String) => {
+        const result = await onMessageChangeDebounced(text);
+        setMessage(result)
     }
     
     return    (
@@ -15,22 +20,24 @@ export function Main() {
             <div className="form-group row">
                 <label className="col-lg-2 control-label">Private Key</label>
                 <div className="col-lg-10 controls">
-                    <textarea className="form-control autoselectall" rows={4} spellCheck='false' placeholder="Paste PGP key here."/>
+                    <Form.Control as="textarea" className="form-control" rows={4} spellCheck='false' placeholder="Paste PGP key here." onFocus={handleFocus}/>
                 </div>
             </div>
             <hr/>
             <div className="form-group row">
                 <label className="col-lg-2 control-label">Private Key</label>
                 <div className="col-lg-10 controls">
-                    <Form.Control as="textarea" className="form-control" rows={8} placeholder="Type your message here" onChange={e=>onMessageChange(e)}/>
+                    <Form.Control as="textarea" className="form-control" rows={8} placeholder="Type your message here" onChange={e=>onMessageChange(e.target.value)}/>
                 </div>
             </div>
-            <div className="form-group row">
+            <div className="form-group row"> 
                 <label className="col-lg-2 control-label">Private Key</label>
                 <div className="col-lg-10 controls">
-                    <textarea className="form-control autoselectall" rows={8} readOnly spellCheck='false' placeholder="Encrypted text will appear here">
-                        {getMessage}
-                    </textarea>
+                    <Form.Control as="textarea" className="form-control autoselectall" rows={8} readOnly spellCheck='false' 
+                        placeholder="Encrypted text will appear here"
+                        onFocus={handleFocus}
+                        value={getMessage}
+                    />
                 </div>
             </div>
         </Form> 
