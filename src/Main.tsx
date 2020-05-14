b561020c34871244308b1431a4f8c5a8e715cc65
EC2 Default User <ec2-user@ip-172-31-21-140.ec2.internal>
2020-05-14 22:51:56 +0000

    Better working prototype

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Alert } from 'react-bootstrap';
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
+import { key }  from 'openpgp'
+
 
 export function Main(props : any) {
-    
     const [getMessage, setMessage] = React.useState("");
+    const [getKey, setKey] = React.useState<key.KeyResult>({'err':null, 'keys':[]});
     
     const handleFocus = (event: any) => event.target.select();
     
@@ -14,24 +16,36 @@ export function Main(props : any) {
         setMessage(result)
     }
     
+    const onKeyChange = async (keyText: string)=>{
+        const mykey = await key.readArmored(keyText);
+        setKey(mykey)
+    }
+    
+    
     return    (
         <Form className="form-horizontal">
             <Form.Label as="legend">Encrypt Message</Form.Label>
             <div className="form-group row">
-                <label className="col-lg-2 control-label">Private Key</label>
+                <label className="col-lg-2 control-label">Public Key</label>
                 <div className="col-lg-10 controls">
-                    <Form.Control as="textarea" className="form-control" rows={4} spellCheck='false' placeholder="Paste PGP key here." onFocus={handleFocus}/>
+                    <Form.Control as="textarea" className="form-control" rows={4} spellCheck='false' placeholder="Paste PGP key here." 
+                    onFocus={handleFocus} onChange={e=>onKeyChange(e.target.value)}/>
                 </div>
+                {getKey.err && getKey.err.map((e,i) =>
+                    <div className="alert" key={i}>
+                        <strong>Warning!</strong> {e.message}
+                    </div>
+                )}
             </div>
             <hr/>
             <div className="form-group row">
-                <label className="col-lg-2 control-label">Private Key</label>
+                <label className="col-lg-2 control-label">Message</label>
                 <div className="col-lg-10 controls">
                     <Form.Control as="textarea" className="form-control" rows={8} placeholder="Type your message here" onChange={e=>onMessageChange(e.target.value)}/>
                 </div>
             </div>
             <div className="form-group row"> 
-                <label className="col-lg-2 control-label">Private Key</label>
+                <label className="col-lg-2 control-label">Result</label>
                 <div className="col-lg-10 controls">
                     <Form.Control as="textarea" className="form-control autoselectall" rows={8} readOnly spellCheck='false' 
                         placeholder="Encrypted text will appear here"
