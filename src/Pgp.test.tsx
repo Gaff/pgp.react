8f360424e5e6b3eeea7cbc994937a87f045df7f8
gaff <use@github>
2020-05-16 05:18:07 +0000

    Add basic pgp tests.

---
+++ src/Pgp.test.tsx
@@ -0,0 +1,22 @@
+import openpgp, { key, message } from 'openpgp'
+import { normal_key } from './testData/testData'
+
+test('Sensible error message',  async () => {
+  const ret = await key.readArmored("foo")
+  const err = ret.err;
+  expect(err).not.toBeNull();
+  if(err) {
+    expect(err.map(x=>x.message)).toEqual(["Misformed armored text"]);
+  }
+});
+
+test('Simple encryption', async() => {
+  const ret = await key.readArmored(normal_key)
+  const publicKey = ret.keys[0]
+  expect(publicKey).not.toBeNull();
+  
+  const data = await openpgp.encrypt({
+      message: message.fromText("Hello world"),
+      publicKeys: publicKey
+  })
+})
